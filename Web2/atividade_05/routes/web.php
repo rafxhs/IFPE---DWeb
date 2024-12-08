7a5fa736feb98dceb5f465503001e6a8f2bbc241
Rafaela Neves <rafaelaneves2712@hotmail.com>
2024-12-07 21:14:23 -0300

    Autenticação, Controllers, Rotas e Views

---
+++ Web2/atividade_05/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', CategoryController::class);
+
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+Route::resource('books', BookController::class)->except(['create', 'store']);
